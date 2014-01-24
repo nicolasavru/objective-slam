0cff3de18eac15a7f53458c17baa4e0e0d790653
Nicolas Avrutin <nicolasavru@gmail.com>
2014-01-23 19:44:02 -0500

    use separate accumulator for each scene reference point

--- drost.m
+++ drost.m
@@ -15,7 +15,7 @@ chair1_vn = vertexNormal(TR);
 
 disp('Chair Model Loaded and Normals Computed');
 
-N = 1000;
+N = 5000;
 
 [scene1_tri,scene1_pts] = ply_read('chair1.ply','tri');
 scene1_pts = roty(1)*rotz(2)*[scene1_pts; ones(1, size(scene1_pts,2))];
@@ -50,19 +50,42 @@ disp('Computed chair map');
 
 [max_alpha, transform_Tmg, transform_Tsg, transform_alpha] = voting_scheme(chairMap, chair1_pts_down, chair1_vn_down, scene1_pts_down, ...
                                        scene1_vn_down, chair_d_dist, chair_d_angle);
-                                     
-transform = invht(transform_Tsg)*transform_alpha*transform_Tmg;                                     
 
-chair_new_pts = transform*[chair1_pts ones(size(chair1_pts,1),1)].';
-chair_new_pts = chair_new_pts(1:3,:).';
-hold on;
-trisurf(chair1_tri,chair1_pts(:,1),chair1_pts(:,2),chair1_pts(:,3), 'EdgeColor', 'green');
-trisurf(chair1_tri,chair_new_pts(:,1),chair_new_pts(:,2),chair_new_pts(:,3));
-trisurf(scene1_tri,scene1_pts(:,1),scene1_pts(:,2),scene1_pts(:,3), ...
-        'EdgeColor', 'none', 'FaceAlpha', 0.8);
-axis equal;
-xlabel('X Axis');
-ylabel('Y Axis');
-zlabel('Z Axis');
+% size(transform_Tsg)
+% size(transform_alpha)
+% size(transform_Tmg)
+% transform_Tmg
 
-                                     
+size(transform_Tmg, 3)
+
+size(transform_Tsg)
+size(transform_alpha)
+size(transform_Tmg)
+
+transform_Tsg
+
+for ii = 1:size(transform_Tmg, 3)
+    transform = invht(transform_Tsg(:,:,ii))*transform_alpha(:,:,ii)*transform_Tmg(:,:,ii);
+
+    if det(transform) > 0
+        ii
+        transform
+        det(transform)
+
+        chair_new_pts = transform*[chair1_pts ones(size(chair1_pts,1),1)].';
+        chair_new_pts = chair_new_pts(1:3,:).';
+
+        hold on;
+        trisurf(chair1_tri,chair1_pts(:,1),chair1_pts(:,2),chair1_pts(:,3), ...
+                'EdgeColor', 'green');
+        trisurf(scene1_tri,scene1_pts(:,1),scene1_pts(:,2),scene1_pts(:,3), ...
+                'EdgeColor', 'none', 'FaceAlpha', 0.8);
+        trisurf(chair1_tri,chair_new_pts(:,1),chair_new_pts(:,2),chair_new_pts(:,3));
+        axis equal;
+        xlabel('X Axis');
+        ylabel('Y Axis');
+        zlabel('Z Axis');
+        hold off;
+        figure;
+    end
+end
--- voting_scheme.m
+++ voting_scheme.m
@@ -13,14 +13,17 @@ index_pairs = [p(:) q(:)];
 Opt.Format = 'hex';
 Opt.Method = 'SHA-1';
 
-accumulator = zeros(size(model_points,1), n_angle);
+accumulator = zeros(size(model_points,1), n_angle, indeces(end));
 % transforms = zeros(size(model_points,1), n_angle, 4, 4);
-transforms_Tmg = zeros(size(model_points,1), n_angle, 4, 4);
-transforms_Tsg = zeros(size(model_points,1), n_angle, 4, 4);
-transforms_alpha = zeros(size(model_points,1), n_angle, 4, 4);
+transforms_Tmg = zeros(size(model_points,1), n_angle, 4, 4, indeces(end));
+transforms_Tsg = zeros(size(model_points,1), n_angle, 4, 4, indeces(end));
+transforms_alpha = zeros(size(model_points,1), n_angle, 4, 4, indeces(end));
+
+I_rows = zeros(indeces(end), 1);
+I_cols = zeros(indeces(end), 1);
+max_tots = zeros(indeces(end), 1);
 
 for ii = 1:size(index_pairs,1)
-  
   if mod(ii, 1000) == 0
     fprintf('On point %d of %d\n', ii, size(index_pairs,1));
   end
@@ -28,25 +31,25 @@ for ii = 1:size(index_pairs,1)
   if index_pairs(ii,1) == index_pairs(ii,2)
     continue
   end
-  
+
   s_r = scene_points(index_pairs(ii,1),:);
   s_i = scene_points(index_pairs(ii,2),:);
   n_r_s = scene_normals(index_pairs(ii,1),:);
-  
+
   F = real(point_pair_feature(s_r, n_r_s, s_i, scene_normals(index_pairs(ii,2),:)));
 %   F_disc = [F(1)-mod(F(1),d_dist); F(2:4)-mod(F(2:4),d_angle)];
   F_disc = [quant(F(1),d_dist); quant(F(2:4),d_angle)];
-  
+
   hash = DataHash(F_disc, Opt);
   key = hex2num(hash(1:16));
-  
+
   if isKey(model_map, key)
     % Model map returns N by 2 array where every row is a set of point pair
     % indeces within model_map
     matched_model_points = model_map(key);
-    
+
     for jj = 1:size(matched_model_points,1)
-      
+
       [T_m_g,T_s_g,alpha] = trans_model_scene( ...
                               model_points(matched_model_points(jj,1),:), ...
                               model_normals(matched_model_points(jj,1),:), ...
@@ -54,29 +57,58 @@ for ii = 1:size(index_pairs,1)
                               s_r, n_r_s, s_i);
                             
 %       alpha_disc = alpha-mod(alpha,d_angle);
-      alpha = real(alpha)
+      alpha = real(alpha);
       alpha_disc = quant(alpha,d_angle)+1;
       alpha_ind = round(alpha_disc/d_angle);
-      
-      accumulator(matched_model_points(jj,1),alpha_ind) = ...
-                    accumulator(matched_model_points(jj,1),alpha_ind) + 1;
-%       transforms(matched_model_points(jj,1),alpha_ind,:,:) = invht(T_s_g)*rotx(alpha)*T_m_g;
-      transforms_Tmg(matched_model_points(jj,1),alpha_ind,:,:) = T_m_g;
-      transforms_Tsg(matched_model_points(jj,1),alpha_ind,:,:) = T_s_g;
-      transforms_alpha(matched_model_points(jj,1),alpha_ind,:,:) = rotx(alpha);
-      
 
+      accumulator(matched_model_points(jj,1),alpha_ind, index_pairs(ii,1)) = ...
+          accumulator(matched_model_points(jj, 1),alpha_ind, index_pairs(ii,1)) + 1;
+%       transforms(matched_model_points(jj,1),alpha_ind,:,:) = invht(T_s_g)*rotx(alpha)*T_m_g;
+      transforms_Tmg(matched_model_points(jj,1), ...
+                     alpha_ind,:,:, index_pairs(ii,1)) = T_m_g;
+      transforms_Tsg(matched_model_points(jj,1), ...
+                     alpha_ind,:,:, index_pairs(ii,1)) = T_s_g;
+      transforms_alpha(matched_model_points(jj, ...
+                                            1),alpha_ind,:,:, index_pairs(ii,1)) = rotx(alpha);
 
     end
+    [Y_rows, I_row] = max(accumulator(:,:,index_pairs(ii,1)));
+    [max_tot, I_col] = max(Y_rows);
+
+    % I_row
+    % I_col
+    % size(I_rows)
+    % size(I_row(I_col))
+
+    I_rows(ii) = I_row(I_col);
+    I_cols(ii) = I_col;
+    max_tots(ii) = max_tot;
   end
 end
 
-[Y_rows, I_row] = max(accumulator);
-[max_tot, I_col] = max(Y_rows);
+[max_tot, max_ind] = max(max_tots);
+I_row = I_rows(max_ind)
+I_col = I_cols(max_ind)
+
+% size(transforms_Tsg)
+% size(transforms_alpha)
+% size(transforms_Tmg)
+
+% for ii = 1:size(transforms_Tmg, 3)
+%     for jj = 1:size(transforms_Tmg, 1)
+%         for kk = 1:size(transforms_Tmg, 2)
+%             squeeze(transforms_Tmg(jj,kk, :, :, ii)
+%         end
+%     end
+% end
 
 % transform = squeeze(transforms(I_row(I_col), I_col, :, :));
-transform1 = squeeze(transforms_Tmg(I_row(I_col), I_col, :, :));
-transform2 = squeeze(transforms_Tsg(I_row(I_col), I_col, :, :));
-transform3 = squeeze(transforms_alpha(I_row(I_col), I_col, :, :));
+transform1 = squeeze(transforms_Tmg(I_row, I_col, :, :, :));
+transform2 = squeeze(transforms_Tsg(I_row, I_col, :, :, :));
+transform3 = squeeze(transforms_alpha(I_row, I_col, :, :, :));
+
+% size(transform1)
+% size(transform2)
+% size(transform3)
 
 end
