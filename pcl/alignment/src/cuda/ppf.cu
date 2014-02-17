09faab322a2a75e0bc1606b592d2ebbc7b4e7115
Nicolas Avrutin <nicolasavru@gmail.com>
2014-02-17 02:38:56 -0500

    implement slam++-style drost model-building

--- pcl/alignment/src/cuda/ppf.cu
+++ pcl/alignment/src/cuda/ppf.cu
@@ -1,28 +1,59 @@
 #include <iostream>
 #include <ctime>
-#include <fstream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <cuda.h>
 #include <cuda_runtime.h>                // Stops underlining of __global__
 #include <device_launch_parameters.h>    // Stops underlining of threadIdx etc.
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <thrust/sort.h>
+#include <thrust/device_ptr.h>
+#include <thrust/inner_product.h>
+#include <thrust/iterator/constant_iterator.h>
+#include <thrust/scan.h>
+#include <math_constants.h>
+#include <functional>
 
 #include "book.h"
+
+#define BLOCK_SIZE 256
+
+__const__ int n_angle = 32;
+__const__ float d_angle = 2*CUDART_PI_F/n_angle;
+__const__ float d_dist = 0.05;
 
 using namespace std;
 
 __device__ __forceinline__ float dot(float3 v1, float3 v2){
-    return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
+    return (v1.x*v2.x + v1.y*v2.y + v1.z*v2.z);
 }
 
 __device__ __forceinline__ float norm(float3 v){
     return sqrtf(dot(v, v));
 }
 
-__device__ float4 compute_ppf(float3 p1, float3 p2, float3 n1, float3 n2){
+// FNV-1a hash function
+// http://programmers.stackexchange.com/questions/49550/which-hashing-algorithm-is-best-for-uniqueness-and-speed
+__device__ unsigned int hash(void *f, int n){
+    char *s = (char *) f;
+    unsigned int hash = 2166136261;
+    while(n--){
+        hash ^= *s++;
+        hash *= 16777619;
+    }
+    return hash;
+}
+
+__device__ float4 disc_feature(float4 f, float d_dist, float d_angle){
+    f.x = f.x - fmodf(f.x, d_dist);
+    f.y = f.y - fmodf(f.y, d_angle);
+    f.z = f.z - fmodf(f.z, d_angle);
+    f.w = f.w - fmodf(f.w, d_angle);
+    return f;
+}
+
+__device__ float4 compute_ppf(float3 p1, float3 n1, float3 p2, float3 n2){
     float3 d;
     d.x = p2.x - p1.x;
     d.y = p2.y - p1.y;
@@ -37,47 +68,197 @@ __device__ float4 compute_ppf(float3 p1, float3 p2, float3 n1, float3 n2){
     return f;
 }
 
-__global__ void ppf_wrapper(float3 *points, float3 *norms, float4 *out, int count){
+__global__ void ppf_kernel(float3 *points, float3 *norms, float4 *out, int count){
     if(count <= 1) return;
 
     int ind = threadIdx.x;
     int idx = ind + blockIdx.x * blockDim.x;
 
     if(idx < count) {
-        float3 thisPoint = points[idx];
-        float3 thisNorm = norms[idx];
 
-        for(int j = 0; j < count; j++) {
-            // if(j == idx) continue;
-            out[ind*count + j] = compute_ppf(thisPoint, points[j], thisNorm, norms[j]);
+        __shared__ float3 Spoints[BLOCK_SIZE];
+        __shared__ float3 Snorms[BLOCK_SIZE];
+
+        float3 thisPoint = points[idx];
+        float3 thisNorm  = norms[idx];
+
+        for(int i = 0; i < count/BLOCK_SIZE; i++){
+
+            Spoints[ind] = points[i*BLOCK_SIZE+ind];
+            Snorms[ind]  = norms[i*BLOCK_SIZE+ind];
+            __syncthreads();
+
+            for(int j = 0; j < BLOCK_SIZE; j++) {
+                if((idx*count + j + i*BLOCK_SIZE) % (count+1) == 0) continue;
+                out[idx*count + j + i*BLOCK_SIZE] = compute_ppf(thisPoint, thisNorm, Spoints[j], Snorms[j]);
+                out[idx*count + j + i*BLOCK_SIZE] = disc_feature(out[idx*count + j + i*BLOCK_SIZE],
+                                                                 d_dist, d_angle);
+            }
         }
     }
 }
 
+// TODO: increase thread work
+__global__ void ppf_encode_kernel(float4 *ppfs, unsigned long *codes, int count){
+    if(count <= 1) return;
 
-void input(int fd, float3 *point){
-    float p[3];
-    read(fd, p, 3*sizeof(float));
-    point->x = p[0];
-    point->y = p[1];
-    point->z = p[2];
+    int ind = threadIdx.x;
+    int idx = ind + blockIdx.x * blockDim.x;
+
+    if(idx < count){
+        unsigned int hk = hash(ppfs+idx, sizeof(float4));
+        codes[idx] = (((unsigned long) hk) << 32) + idx;
+    }
+}
+
+// TODO: increase thread work
+__global__ void ppf_decode_kernel(unsigned long *codes, unsigned int *key2ppfMap,
+                                  unsigned int *hashKeys, int count){
+    if(count <= 1) return;
+
+    int ind = threadIdx.x;
+    int idx = ind + blockIdx.x * blockDim.x;
+
+    unsigned long low32 = ((unsigned long) -1) >> 32;
+
+    if(idx < count){
+        // line 11 in algorithm 1, typo on their part?!?
+        key2ppfMap[idx] = (unsigned int) (codes[idx] & low32);
+        hashKeys[idx] = (unsigned int) (codes[idx] >> 32);
+    }
+}
+
+// TODO: Re-write as a class. Have the constructor take n as an
+// argument and cudaMalloc() everything. Have the destructor
+// cudaFree() everything. Have ppf_lookup be a method. Replace
+// (unsigned int *)s with thrust::device_vectors and modify everything
+// else accordingly.
+//
+// structure for searching a model description
+struct search_structure{
+    // Number of PPF in the mode. I.e., number of elements in each of
+    // the following arrays;
+    int n;
+
+    // List of all hash keys. Use a parallel binary search to find
+    // index of desired hash key.
+    unsigned int *hashKeys;
+
+    // ppfCount[i] is the number of PPFs whose hash is hashKeys[i];
+    unsigned int *ppfCount;
+
+    // firstPPFIndex[i] is the index of the first entry in key2ppfMap
+    // corresponding to hashKey[i]. The following ppfCount[i]-1
+    // entries also correspond to hashKey[i].
+    unsigned int *firstPPFIndex;
+
+    // key2ppfMap[i] is the index in d_ppfs that contains (one of) the
+    // PPF(s) whose hash is hashKeys[i]. From there, the indices of
+    // the points that were used to generate the PPF can be
+    // calculated.
+    unsigned int *key2ppfMap;
+};
+
+// TODO: finish
+//
+// TODO: redo using thrust vectorized search
+// __device__ int ppf_lookup(struct search_structure s, float4 *ppf, float4 *results){
+//     int hashKey = hash(ppf, sizeof(float4));
+//     int hash_idx;
+//     // Iterator iter = thrust::lower_bound(thrust::device, s.hashKeys, s.hashKeys + n, hashKey);
+
+//     return 0;
+// }
+
+struct search_structure build_model_description(float4 *d_ppfs, int n){
+    // for each ppf, compute a 32-bit hash and concatenate it with
+    // a 32-bit int representing the index of the ppf in d_ppfs
+    unsigned long *d_codes;
+    HANDLE_ERROR(cudaMalloc(&d_codes, n*sizeof(unsigned long)));
+    thrust::device_ptr<unsigned long> dev_ptr(d_codes); 
+
+    ppf_encode_kernel<<<n/BLOCK_SIZE,BLOCK_SIZE>>>(d_ppfs, d_codes, n);
+    thrust::sort(dev_ptr, dev_ptr+n);
+
+
+    // split codes into array of hashKeys (high 32 bits) and
+    // key2ppfMap, the associated indices (low 32 bits)
+    unsigned int *key2ppfMap, *hashKeys;
+    HANDLE_ERROR(cudaMalloc(&key2ppfMap, n*sizeof(unsigned int)));
+    HANDLE_ERROR(cudaMalloc(&hashKeys, n*sizeof(unsigned int)));
+    thrust::device_ptr<unsigned int> hashKeys_ptr(hashKeys);
+    thrust::device_ptr<unsigned int> key2ppfMap_ptr(key2ppfMap);
+
+    ppf_decode_kernel<<<n/BLOCK_SIZE,BLOCK_SIZE>>>(d_codes, key2ppfMap, hashKeys, n);
+    cudaFree(d_codes);
+
+
+    // create histogram of hash keys
+    // https://code.google.com/p/thrust/source/browse/examples/histogram.cu
+    unsigned int num_bins = thrust::inner_product(hashKeys_ptr, hashKeys_ptr + n - 1,
+                                                  hashKeys_ptr + 1,
+                                                  (unsigned int) 1,
+                                                  thrust::plus<unsigned int>(),
+                                                  thrust::not_equal_to<unsigned int>());
+
+    /* DEBUG */
+    fprintf(stderr, "num_bins: %d\n", num_bins);
+    /* DEBUG */
+
+    unsigned int *hashKeys_new, *ppfCount;
+    HANDLE_ERROR(cudaMalloc(&hashKeys_new, num_bins*sizeof(unsigned int)));
+    HANDLE_ERROR(cudaMalloc(&ppfCount, num_bins*sizeof(unsigned int)));
+    thrust::device_ptr<unsigned int> hashKeys_new_ptr(hashKeys_new);
+    thrust::device_ptr<unsigned int> ppfCount_ptr(ppfCount);
+
+    thrust::reduce_by_key(hashKeys_ptr, hashKeys_ptr + n,
+                          thrust::constant_iterator<unsigned int>(1),
+                          hashKeys_new_ptr,
+                          ppfCount_ptr);
+    cudaFree(hashKeys);
+
+
+    // create list of beginning indices of blocks of ppfs having equal hashes
+    unsigned int *firstPPFIndex;
+    HANDLE_ERROR(cudaMalloc(&firstPPFIndex, num_bins*sizeof(unsigned int)));
+    thrust::device_ptr<unsigned int> firstPPFIndex_ptr(firstPPFIndex);
+
+    thrust::exclusive_scan(ppfCount_ptr, ppfCount_ptr+num_bins, firstPPFIndex_ptr);
+
+    struct search_structure result;
+    result.n = n;
+    result.hashKeys = hashKeys_new;
+    result.ppfCount = ppfCount;
+    result.firstPPFIndex = firstPPFIndex;
+    result.key2ppfMap = key2ppfMap;
+    return result;
 }
 
 int ply_load_main(char *point_path, char *norm_path, int N){
-    int points_fin = open(point_path, O_RDONLY);    // read in points
-    int norms_fin = open(norm_path, O_RDONLY);    // read in norms
-    // Array of points
+    // file input
+    FILE *points_fin, *norms_fin;
+    size_t result1, result2;
+
+    points_fin = fopen(point_path, "rb");
+    norms_fin  = fopen(norm_path, "rb");
+    if(points_fin==NULL){fputs ("File error: point_fin",stderr); exit (1);}
+    if(norms_fin==NULL){fputs ("File error: norms_fin",stderr); exit (1);}
+
     float3 *points = new float3[N];
     float3 *norms = new float3[N];
+    if (points == NULL) {fputs ("Memory error: points",stderr); exit (2);}
+    if (norms  == NULL) {fputs ("Memory error: norms",stderr); exit (2);}
 
-    // read in data and time
-    long startTime = clock();
-    for(int i = 0; i < N; i++){
-        input(points_fin, points+i);
-        input(norms_fin, norms+i);
-    }
-    long finishTime = clock();
+    long startTime0 = clock();
+    result1 = fread(points,sizeof(float3),N,points_fin);
+    result2 = fread(norms,sizeof(float3),N,norms_fin);
+    long finishTime0 = clock();
 
+    if(result1 != N){fputs ("Reading error: points",stderr); exit (3);}
+    if(result2 != N){fputs ("Reading error: norms",stderr); exit (3);}
+
+
+    // cuda setup
     cudaDeviceProp  prop;
     HANDLE_ERROR(cudaGetDeviceProperties(&prop, 0));
     int blocks = prop.multiProcessorCount;
@@ -85,40 +266,46 @@ int ply_load_main(char *point_path, char *norm_path, int N){
     fprintf(stderr, "blocks: %d\n", blocks);
     /* DEBUG */
 
+
+    // start cuda timer
     cudaEvent_t start, stop;
     HANDLE_ERROR(cudaEventCreate(&start));
     HANDLE_ERROR(cudaEventCreate(&stop));
     HANDLE_ERROR(cudaEventRecord(start, 0));
 
-    float3 *d_points; // GPU version
-    float3 *d_norms; // GPU version
-    float4 *ppfs = new float4[N*N];
-    float4 *d_ppfs; // GPU version
 
-    int page_size = min(N, 4096);
+    // compute ppfs
+    float3 *d_points, *d_norms;
+    float4 *d_ppfs;
     HANDLE_ERROR(cudaMalloc(&d_points, N*sizeof(float3)));
     HANDLE_ERROR(cudaMalloc(&d_norms, N*sizeof(float3)));
-    HANDLE_ERROR(cudaMalloc(&d_ppfs, page_size*page_size*sizeof(float4)));
+    HANDLE_ERROR(cudaMalloc(&d_ppfs, N*N*sizeof(float4)));
 
     HANDLE_ERROR(cudaMemcpy(d_points, points, N*sizeof(float3), cudaMemcpyHostToDevice));
     HANDLE_ERROR(cudaMemcpy(d_norms, norms, N*sizeof(float3), cudaMemcpyHostToDevice));
 
-    for(int offset = 0; offset < N; offset+=page_size){
-        // call ppf kernel
-        ppf_wrapper<<<1,1024>>>(d_points+offset, d_norms+offset, d_ppfs, page_size);
-        HANDLE_ERROR(cudaMemcpy(ppfs+offset, d_ppfs, page_size*page_size*sizeof(float4), cudaMemcpyDeviceToHost));
-    }
+    ppf_kernel<<<N/BLOCK_SIZE,BLOCK_SIZE>>>(d_points, d_norms, d_ppfs, N);
 
-    // end timer
+
+    // build model description
+    struct search_structure model;
+    model = build_model_description(d_ppfs, N*N);
+
+
+    // end cuda timer
     HANDLE_ERROR(cudaEventRecord(stop, 0));
     HANDLE_ERROR(cudaEventSynchronize(stop));
     float elapsedTime;
     HANDLE_ERROR(cudaEventElapsedTime(&elapsedTime, start, stop));
     printf("Time to generate:  %3.1f ms\n", elapsedTime);
 
+
+    // copy ppfs back to host
+    float4 *ppfs = new float4[N*N];
+    HANDLE_ERROR(cudaMemcpy(ppfs, d_ppfs, N*N*sizeof(float4), cudaMemcpyDeviceToHost));
+
     // write out ppfs
-    for(int i = 0; i < 10; i++)    // loop over rows
-    {
+    for(int i = 0; i < 100; i++){
         cout << "PPF Number: " << i << endl;
         cout << ppfs[i].x << endl;
         cout << ppfs[i].y << endl;
@@ -126,7 +313,7 @@ int ply_load_main(char *point_path, char *norm_path, int N){
         cout << ppfs[i].w << endl;
     }
 
-    cout<<"Data Load Time"<<" "<<(finishTime - startTime)<<" ms"<<endl;
+    cout<<"Data Load Time"<<" "<<(finishTime0 - startTime0)<<" ms"<<endl;
 
     // Deallocate ram
     delete[] points;
@@ -137,11 +324,16 @@ int ply_load_main(char *point_path, char *norm_path, int N){
     cudaFree(d_norms);
     cudaFree(d_ppfs);
 
+    cudaFree(model.hashKeys);
+    cudaFree(model.key2ppfMap);
+    cudaFree(model.ppfCount);
+    cudaFree(model.firstPPFIndex);
+
     cudaDeviceReset();
 
     // close input file
-    close(points_fin);
-    close(norms_fin);
+    fclose(points_fin);
+    fclose(norms_fin);
     return 0;
 }
 
